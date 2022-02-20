c5683fc7a83373f7503f467f83d7ba33f893112a
MaxoumeGit <71985501+MaxoumeGit@users.noreply.github.com>
2022-02-20 15:24:34 +0100

    tests

---
+++ js/index.js
@@ -0,0 +1,10 @@
+import "./styles.scss";
+
+const logo = document.getElementById("logo");
+
+document.onmousemove = ev => {
+  const positionX = (window.innerWidth / 2 - ev.x) / 4;
+  const positionY = -ev.y / 5;
+  console.log(positionX, positionY);
+  logo.style.transform = `translate(${positionX}px, ${positionY}px)`;
+};
